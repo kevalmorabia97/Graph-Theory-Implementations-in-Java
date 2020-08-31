573ebf8f17a99eaea3855a4e998800834b5e8398
Joselyne Isabel Torres Duque <57409647+JRush25@users.noreply.github.com>
2020-08-30 23:47:53 -0500

    Update BreadthFirstSearch.java

--- src/BreadthFirstSearch.java
+++ src/BreadthFirstSearch.java
@@ -1,8 +1,9 @@
 import java.util.LinkedList;
 
 public class BreadthFirstSearch {
-	public BreadthFirstSearch(int v) {
-		super(v);
+	Graph g;
+	public BreadthFirstSearch(Graph g) {
+		this.g = g;
 	}
 
 	void BFS(int start){
